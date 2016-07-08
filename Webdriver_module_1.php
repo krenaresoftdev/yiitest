5330f3e3b4f77b0621c73a03dfbe9b47ad61281e
krenaresoftdev <krenare.softdev@pecb.com>
2016-07-08 16:49:03 +0200

    Add files via upload

---
+++ Webdriver_module_1.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Webdriver_module_1
+ *
+ * @author PECB-106
+ */
+class Webdriver_module_1 {
+    //put your code here
+}
---
+++ calc.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of calc
+ *
+ * @author PECB-106
+ */
+class calc {
+    //put your code here
+    /**
+     * @assert (0, 0) == 0
+     * @assert (0, 1) == 1
+     * @assert (1, 0) == 1
+     * @assert (1, 1) == 2
+     * @assert (1, 2) == 4
+     */
+    public function add($a, $b)
+    {
+        return $a + $b;
+    }
+}
